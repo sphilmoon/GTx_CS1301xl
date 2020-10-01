a7a96aca8fb0ca4dc8772778c3987a501d1718f2
sphilmoon <sphilmoon@gmail.com>
2020-10-01 16:40:30 +0900

    True or False with comparing str or int variables.

---
+++ 2.3.3_SetOperators1.py
@@ -0,0 +1,7 @@
+myletter = "Hey, there!"
+
+# using an 'in' reserve word in Python.
+print("H" in myletter) # True
+print("there!" in myletter) # True
+print("yeH" in myletter) # False. Not in this order.
+print("" in myletter) # there is nothing to find. But weridly this is True!
